0d37878fa4ccc5e8526163851bd75e9845f6ca11
jackmappotion <jackmappotion@gmail.com>
2024-02-10 09:57:17 +0900

    :heavy_plus_sign: Ad
    add blp

---
+++ blueprint/position_generator.py
@@ -0,0 +1,17 @@
+"""
+Position Generator
+
+Options : 
+    - Price
+    - High / Low
+    - Volume
+    
+    - length_limit
+    - time_limit
+
+[price / length_limit]
+[price / ]
+
+"""
+
+class PositionGenerator:
